0c31042c7aec9bad4368764f2bfde3b389bd0e74
Ben Allfree <ben@benallfree.com>
2023-09-21 04:31:51 -0700

    fix scoping bug

--- src/entries/index.pb.ts
+++ src/entries/index.pb.ts
@@ -1,3 +1,5 @@
-onAfterBootstrap(require(`${__hooks}/boot-message.js`).default)
+onAfterBootstrap(() => require(`${__hooks}/boot-message.js`).default())
 
-routerAdd(`GET`, `/api/hello/:name`, require(`${__hooks}/hello.js`).default)
+routerAdd(`GET`, `/api/hello/:name`, (c) =>
+  require(`${__hooks}/hello.js`).default(c)
+)
